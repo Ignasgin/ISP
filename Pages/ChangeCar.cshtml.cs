4e110338bc0a5ad4d8455cf3635fcd171d803f0d
ignasgin <ignasgin@gmail.com>
2024-11-08 18:13:36 +0200

    Title page and the car list page

---
+++ Pages/ChangeCar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ISP.Pages
+{
+    public class ChangeCarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
