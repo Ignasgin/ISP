b0635fd30bbaf7aa3eff4d2c9bb790f83e7871ef
BryaN <60313619+BryanLTU@users.noreply.github.com>
2024-12-15 17:35:36 +0200

    feat: Reservations check service

---
+++ Data/ReservationCheckService.cs
@@ -0,0 +1,48 @@
+﻿namespace ISP.Data
+{
+    public class ReservationCheckService : IHostedService, IDisposable
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private Timer _timer;
+
+        public ReservationCheckService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(CheckReservations, null, TimeSpan.Zero, TimeSpan.FromMinutes(10));
+
+            return Task.CompletedTask;
+        }
+
+        private async void CheckReservations(object state)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                var reservations = context.Trumpalaike_Rezervacija
+                    .Where(r => r.Pateikimo_Data.AddHours(r.Laikas) <= DateTime.Now).ToList();
+
+                if (reservations.Any())
+                {
+                    context.Trumpalaike_Rezervacija.RemoveRange(reservations);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
