16782028e4c55fb445fc20e3ce8ef2cdcb57c202
BryaN <60313619+BryanLTU@users.noreply.github.com>
2024-12-15 17:36:10 +0200

    feat: Reservations check service (#6)

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHostedService<ReservationCheckService>();
 builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
